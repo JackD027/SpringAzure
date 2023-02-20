634a309e59c7e60a16169157cccdc5f149fd25a8
Shubhendu Shekhar <66331629+JackD027@users.noreply.github.com>
2023-02-20 18:43:01 +0530

    Update Controller.java

--- src/main/java/com/nagarro/spring/controller/Controller.java
+++ src/main/java/com/nagarro/spring/controller/Controller.java
@@ -11,7 +11,32 @@ public class Controller {
 	
 	@GetMapping("/api")
 	public String getString () {
-		return "Hi from Controller";
-		
+		return 
+		"[
+    {
+        "id": 1,
+        "firstName": "Aman",
+        "lastName": "Singh",
+        "exp": 3,
+        "dept": "IT",
+        "salary": 1000
+    },
+    {
+        "id": 2,
+        "firstName": "Naman",
+        "lastName": "Singh",
+        "exp": 5,
+        "dept": "Sales",
+        "salary": 4000
+    },
+    {
+        "id": 3,
+        "firstName": "Chaman",
+        "lastName": "Singh",
+        "exp": 4,
+        "dept": "Management",
+        "salary": 5000
+    }
+]";
 	}
 }
